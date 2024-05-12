f529dc675d29768bc072ac07916f7450a73e0c99
Lucas Rocha <lucasrocha94.dev@gmail.com>
2024-05-11 23:21:08 -0300

    centralizing things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
     const collumns = await getCollumns();
 
     return (
-        <div className="min-h-full-without-header max-h-full-without-header flex gap-5 py-6 overflow-hidden">
+        <div className="min-h-full-without-header max-h-full-without-header flex justify-center gap-5 py-6 overflow-hidden">
             {collumns?.map((collumn) => (
                 <CardsCollum key={collumn.id} collumn={collumn} />
             ))}
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -4,7 +4,9 @@ const Header = () => {
     return (
         <div className="bg-gray-900 w-full text-white min-h-16">
             <div className="max-w-screen-2xl flex justify-between items-center min-h-16 mx-auto">
-                <span>Kanban</span>
+                <Link href={"/"}>
+                    <span>Kanban</span>
+                </Link>
                 <nav>
                     <ul>
                         <li className="p-2 w-32 flex justify-center items-center bg-cyan-300 rounded-xl text-black ">
